2585c4cc07a80943bcf9df877978ec5e497a0e33
Rohan Bansal <rohan@digithinkit.com>
2019-05-03 16:36:07 +0530

    feat(compliance): Add feature for cannabis waste disposal

---
+++ bloomstack_core/public/js/batch.js
@@ -0,0 +1,26 @@
+/* global frappe, erpnext, _ */
+
+frappe.ui.form.on('Batch', {
+	refresh: (frm) => {
+		if (frm.doc.expiry_date < frappe.datetime.now_date()) {
+			frm.add_custom_button(__("Move to Waste"), () => {
+				frappe.prompt({
+					label: "Target Warehouse",
+					fieldname: "warehouse",
+					fieldtype: "Link",
+					options: "Warehouse",
+					reqd: 1
+				},
+				function (data) {
+					frappe.model.open_mapped_doc({
+						method: "bloomstack_core.utils.move_expired_batches",
+						frm: frm,
+						args: {
+							warehouse: data.warehouse
+						}
+					});
+				}, __("Select Warehouse"), __("Move"));
+			});
+		}
+	}
+});
