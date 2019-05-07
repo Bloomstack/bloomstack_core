58539c53859f2ce3a971c05b2f080b9a7b683f74
Rohan Bansal <rohan@digithinkit.com>
2019-05-07 13:44:06 +0530

    fix(metrc): Only use METRC hooks if METRC is enabled in the system

--- bloomstack_core/bloomstack_core/doctype/compliance_settings/compliance_settings.js
+++ bloomstack_core/bloomstack_core/doctype/compliance_settings/compliance_settings.js
@@ -2,7 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Compliance Settings', {
-	refresh: function(frm) {
+	is_metrc_enabled: (frm) => {
+		frm.trigger("toggle_metrc_fields");
+	},
 
+	toggle_metrc_fields: (frm) => {
+		frm.toggle_reqd(["metrc_url", "metrc_license_no", "metrc_vendor_key", "metrc_user_key"], frm.doc.is_metrc_enabled);
 	}
 });
