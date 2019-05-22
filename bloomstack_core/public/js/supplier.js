ebcb1685891f92ec50a7a2ade42add469d0dc88b
Valmik Jangla <mail@valmik.in>
2019-05-22 15:47:38 +0530

    fix(compliance): hide Compliance Info button when creating new documents

--- bloomstack_core/public/js/supplier.js
+++ bloomstack_core/public/js/supplier.js
@@ -1,7 +1,9 @@
 frappe.ui.form.on("Supplier", {
 	refresh: (frm) => {
-		frm.add_custom_button(__("License Info"), () => {
-			frappe.set_route("List", "Compliance Info", { "entity": frm.doc.name })
-		})
+		if (!frm.is_new()) {
+			frm.add_custom_button(__("License Info"), () => {
+				frappe.set_route("List", "Compliance Info", { "entity": frm.doc.name })
+			})
+		}
 	}
 })
