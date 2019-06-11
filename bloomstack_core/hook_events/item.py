6e3a40b998835b1f961930a10697a54d4840eb13
Rohan Bansal <rohan@digithinkit.com>
2019-06-11 14:41:53 +0530

    feat(stock): Call autoname on Item form

--- bloomstack_core/hook_events/item.py
+++ bloomstack_core/hook_events/item.py
@@ -1,3 +1,5 @@
+import json
+
 from six import string_types
 
 import frappe
@@ -5,19 +7,28 @@ from erpnext import get_default_company
 from erpnext.accounts.utils import get_company_default
 
 
-def autoname(item, method):
+@frappe.whitelist()
+def autoname_item(item):
+	item = frappe._dict(json.loads(item))
+	item_code = autoname(item)
+
+	return item_code
+
+
+def autoname(item, method=None):
 	"""
 		Item Code = a + b + c + d + e, where
-			a = abbreviated Company; first 3 letters - all caps.
-			b = abbreviated Brand; first 2 letters - all caps.
-			c = abbreviated Item Group; first 2 letters - all caps.
-			d = abbreviated Item Name; first 3 letters - all caps.
+			a = abbreviated Company; all caps.
+			b = abbreviated Brand; all caps.
+			c = abbreviated Item Group; all caps.
+			d = abbreviated Item Name; all caps.
 			e = variant ID number; has to be incremented.
 	"""
 
 	# Get abbreviations
 	company_abbr = get_company_default(get_default_company(), "abbr")
-	brand_abbr = get_abbr(item.brand)
+	brand_abbr = get_abbr(item.brand, max_length=len(company_abbr))
+	brand_abbr = brand_abbr if company_abbr != brand_abbr else None
 	item_group_abbr = get_abbr(item.item_group)
 	item_name_abbr = get_abbr(item.item_name, 3)
 
@@ -32,20 +43,21 @@ def autoname(item, method):
 	item_code = "-".join([item_code, count])
 	item.name = item.item_code = item_code
 
+	return item_code
+
 
 def get_abbr(txt, max_length=2):
-	from six import string_types
-	abbr = ""
-
 	if not isinstance(txt, string_types):
-		return abbr
+		return
 
+	abbr = ""
 	words = txt.split(" ")
+
 	if len(words) > 1:
 		# Use initials for multi-word texts
 		for word in words:
 			if len(abbr) >= max_length:
-				return abbr
+				break
 
 			if word.strip():
 				abbr += word.strip()[0]
