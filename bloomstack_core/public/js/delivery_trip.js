d6583fb914d07d15b8ea176be84ce911ee6d0194
diksha <diksha@gmail.com>
2019-06-13 14:48:11 +0530

    feat(core): refactor code

--- bloomstack_core/public/js/delivery_trip.js
+++ bloomstack_core/public/js/delivery_trip.js
@@ -10,13 +10,10 @@ frappe.ui.form.on('Delivery Trip', {
                 },
                 ],
                     function (data) {
-                        frappe.call({
-                            method: "bloomstack_core.utils.update_odometer",
-                            args: {
-                                "start": data.odometer_start_value,
-                                "dn": frm.docname,
-                            },
-                        });
+                        frm.set_value('odometer_start_value', data.odometer_start_value);
+                        frm.set_value('odometer_start_time', frappe.datetime.now_datetime());
+                        frm.dirty();
+                        frm.save_or_update();
                     },
                     __("Enter Odometer Value"));
 
@@ -24,6 +21,7 @@ frappe.ui.form.on('Delivery Trip', {
         }
         else if (frm.doc.docstatus == 1 && frm.doc.status != "Completed" && frm.doc.odometer_start_value > 0 && frm.doc.odometer_stop_value == 0) {
             frm.add_custom_button(__("Stop"), () => {
+
                 frappe.prompt([{
                     "label": "Odometer Stop Value",
                     "fieldtype": "Int",
@@ -32,18 +30,16 @@ frappe.ui.form.on('Delivery Trip', {
                 },
                 ],
                     function (data) {
-                        frappe.call({
-                            method: "bloomstack_core.utils.update_odometer",
-                            args: {
-                                "stop": data.odometer_stop_value,
-                                "dn": frm.docname,
-                            },
-
-                        });
+                        frm.set_value('odometer_stop_value', data.odometer_stop_value);
+                        frm.set_value('odometer_stop_time', frappe.datetime.now_datetime());
+                        frm.set_value('actual_distance_travelled', (data.odometer_stop_value - frm.doc.odometer_start_value));
+                        frm.dirty();
+                        frm.save_or_update();
                     },
                     __("Enter Odometer Value"));
 
             }).addClass("btn-primary");
         }
+
     }
 });
