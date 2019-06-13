60ab419897806e004e4eeeca9abfe3ee4450272a
Rohan Bansal <rohan@digithinkit.com>
2019-06-13 14:37:46 +0530

    fix(stock): Add docstring to abbreviation function

--- bloomstack_core/hook_events/item.py
+++ bloomstack_core/hook_events/item.py
@@ -47,14 +47,32 @@ def autoname(item, method=None):
 
 
 def get_abbr(txt, max_length=2):
-	if not isinstance(txt, string_types):
+	"""
+		Extract abbreviation from the given string as:
+			- Single-word strings abbreviate to the letters of the string, upto the max length
+			- Multi-word strings abbreviate to the initials of each word, upto the max length
+
+	Args:
+		txt (str): The string to abbreviate
+		max_length (int, optional): The max length of the abbreviation. Defaults to 2.
+
+	Returns:
+		str: The abbreviated string, in uppercase
+	"""
+
+	if not txt:
 		return
+
+	if not isinstance(txt, string_types):
+		try:
+			txt = str(txt)
+		except:
+			return
 
 	abbr = ""
 	words = txt.split(" ")
 
 	if len(words) > 1:
-		# Use initials for multi-word texts
 		for word in words:
 			if len(abbr) >= max_length:
 				break
@@ -62,7 +80,6 @@ def get_abbr(txt, max_length=2):
 			if word.strip():
 				abbr += word.strip()[0]
 	else:
-		# Use letters (upto max length) in the given string
 		abbr = txt[:max_length]
 
 	abbr = abbr.upper()
