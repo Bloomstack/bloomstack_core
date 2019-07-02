5684bdb82f5137923e1efcb4a59820757dbc52c3
Felipe Orellana <forellana-digithinkit@users.noreply.github.com>
2019-07-02 03:09:40 -0400

    Update bloomstack_core/services/issues.py
    Co-Authored-By: Rohan <Alchez@users.noreply.github.com>

--- bloomstack_core/services/issues.py
+++ bloomstack_core/services/issues.py
@@ -6,4 +6,4 @@ def issueStatusList():
 	Returns an array of statuses available on the Issues doctype.
 	"""
 	meta = frappe.get_meta("Issue")
-	return meta.get_field("status").options.split("\n")
+	return meta.get_options("status").split("\n")
