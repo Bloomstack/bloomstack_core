a9d191fab70b3f76895574110dade0a3133fc9d6
Rohan <rohan@digithinkit.com>
2019-07-23 21:27:29 +0530

    fix: change Insight Engine label to Dashboard

--- bloomstack_core/bloomstack_core/page/insight_engine/insight_engine.js
+++ bloomstack_core/bloomstack_core/page/insight_engine/insight_engine.js
@@ -3,7 +3,7 @@
 frappe.pages['insight-engine'].on_page_load = function(wrapper) {
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'Insight Engine',
+		title: 'Dashboard',
 		single_column: true
 	});
 
