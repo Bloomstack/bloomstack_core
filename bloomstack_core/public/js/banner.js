77ab59f51e82b4dd149c8396dcbc1efa09538baa
diksha <diksha@gmail.com>
2019-08-10 01:13:43 +0530

    feat: add close button on notice

--- bloomstack_core/public/js/banner.js
+++ bloomstack_core/public/js/banner.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
     if (frappe.boot.staging_server == true) {
-        $('body').append($('<div class="bloomstack-banner"><button type="button" class="btn close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button> </div>'));
-        // get banner ref
-        let $banner = $('.bloomstack-banner');
-
-        // empty banner in case there was something in it before hand:
-        $banner.empty();
-
-        // add your content
-        $banner.text("Notice : This is a staging instance");
-
+    let $banner = $(
+        `<div class="bloomstack-banner">
+            <span class="notice">Notice : This is a staging instance</span>
+            <button type="button" class="btn close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">×</span>
+            </button>
+        </div>`
+     );    
+    $('body').append($banner);
+    $banner.find("button").click(function(){
+        $banner.remove();
+    });
         // and finally display it:
-        $('body').addClass('has-banner')
-    }
+        $('body').addClass('has-banner');
+}
 });
