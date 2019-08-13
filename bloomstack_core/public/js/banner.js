1fac2514dc1940ed3a60ad37e93b8f941a477ae4
diksha <diksha@gmail.com>
2019-08-13 15:32:03 +0530

    fix: change the color of close button to white

--- bloomstack_core/public/js/banner.js
+++ bloomstack_core/public/js/banner.js
@@ -3,9 +3,6 @@ $(document).ready(function() {
     let $banner = $(
         `<div class="bloomstack-banner">
             <span class="notice">Notice : This is a staging instance</span>
-            <button type="button" class="btn close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">×</span>
-            </button>
         </div>`
      );    
     $('body').append($banner);
