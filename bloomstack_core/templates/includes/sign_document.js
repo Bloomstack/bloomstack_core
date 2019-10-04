d5e84e5d4ca6d75fd8b130495112d0c2560818d7
Diksha <dikshajadhav11.dj@gmail.com>
2019-10-04 22:41:19 +0530

    feat(authorization request): show signed copy of doc and show print and download buttons when signed

--- bloomstack_core/templates/includes/sign_document.js
+++ bloomstack_core/templates/includes/sign_document.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
         var signee = $("#signee").val();
         if ($sigdiv.jSignature('getData', 'native').length != 0 && signee) {   // proceed only if user has put signature and signee name.
             $(".user-signature").hide();
+            $(".contract").hide();
+
             frappe.call({
                 method: "bloomstack_core.utils.authorize_document",
                 args: {
@@ -17,11 +19,36 @@ $(document).ready(function () {
                 },
                 freeze: true,
                 callback: (r) => {
-                    window.open('/api/method/frappe.utils.print_format.download_pdf?' +
-							'doctype=' + encodeURIComponent($('#doctype').data().name) +
-							'&name=' + encodeURIComponent($('#docname').data().name) +
-                            '&format=' + encodeURIComponent(""), "_self");
-                    frappe.msgprint(__("The document has been approved by you!"));
+                    frappe.call({
+						method: "bloomstack_core.www.authorize_document.custom_print_doc",
+						args: {
+							auth_req_docname: "{{ auth_req_docname }}"
+                        },
+						callback: (r) => {
+							$(".title").html("You have signed the below {{doc.doctype}}!");
+							$(".contract").html(r.message);
+                            $(".contract").show();
+                            $(".signed-doc-actions").show();
+                            // $(".signed-doc-actions").css('visibility', 'visible');
+                            // $(".signed-doc-actions").style.visibility = 'visible';
+                            // $(".signed-doc-actions").removeClass("hidden").addClass("shown");
+                            // $(".signed-doc-actions").toggleClass('hidden')
+							// $(".contract").html(r.message);
+						}
+					})
+                    // console.log("doctype", $('#doctype').data().name);
+                    // console.log("docname", $('#docname').data().name);
+                    // console.log("doctype", $('#doctype').data());
+                    // console.log("docname", $('#docname').data());
+                    // window.open('/api/method/frappe.utils.print_format.download_pdf?' +
+					// 		'doctype=' + encodeURIComponent($('#doctype').data("dt")) +
+					// 		'&name=' + encodeURIComponent($('#docname').data("name")) +
+                    //         '&format=' + encodeURIComponent(""), "_self");
+                    // // frappe.msgprint(__("The document has been approved by you!"));
+
+                    // console.log("$('#doctype').data('dt')", $('#doctype').data("dt"));
+                    // console.log("$('#docname').data('name')", $('#docname').data("name"));
+                   
                 }
             });
         }
@@ -31,6 +58,11 @@ $(document).ready(function () {
         }
     });
 
+    $("#printBtn").on("click", function () {
+        <a class='text-muted small' href='/printview?doctype={{ doc.doctype}}&name={{ doc.name }}
+        {%- if print_format -%}&format={{ Web Contract }}{%- endif -%}' target="_blank" rel="noopener noreferrer">
+        <i class='fa fa-print'></i> {{ _('Print') }}</a>
+    });
     $("#rejectDocument").on("click", function () {
         $(".user-signature").hide();
         frappe.call({
--- bloomstack_core/www/authorize_document.py
+++ bloomstack_core/www/authorize_document.py
@@ -27,3 +27,14 @@ def get_context(context):
     context.doc = frappe.get_doc(auth_req.linked_doctype, auth_req.linked_docname)
     context.company = context.doc.company if hasattr(context.doc, 'company') else get_default_company()
     context.auth_req_docname = docname
+
+
+@frappe.whitelist()
+def custom_print_doc(auth_req_docname):
+    auth_req = frappe.get_doc("Authorization Request", auth_req_docname)
+
+    print_doc = frappe.get_print(auth_req.linked_doctype, auth_req.linked_docname, "Web Contract")
+    custom = print_doc[print_doc.find('<body>') + len('<body>'):len(print_doc) - len('</body>')]
+    print("print_doc", print_doc)
+    print("custom", custom)
+    return custom
