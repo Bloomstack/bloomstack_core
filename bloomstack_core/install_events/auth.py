0ce107f40be920f248d4be0c5009ef505ea8ee24
Rohan Bansal <rohan@digithinkit.com>
2019-10-25 13:56:01 +0530

    feat: create social login keys for Bloomstack and Frappe

--- bloomstack_core/install_events/auth.py
+++ bloomstack_core/install_events/auth.py
@@ -3,18 +3,32 @@ import json
 import requests
 
 import frappe
+from frappe import _
+from frappe.utils import get_url
+
+
+class ClientRegistrationError(frappe.ValidationError): pass
 
 
 def after_install():
-	create_client()
+	"""
+		After the site is successfully created, we register the company on
+		Bloomstack's authentication servers, and create login integrations
+		and an OAuth client for the company.
+	"""
+
+	client_info = register_client()
+	create_social_login_keys(client_info)
+	create_oauth_client(client_info)
 
 
-def create_client():
+def register_client():
 	site = frappe.local.site
 	base_url = frappe.local.conf.get("auth_server")
 	company_name = frappe.local.conf.get("company_name")
+	redirect_uri = frappe.local.conf.get("oauth_login_redirect_uri")
 
-	redirect_uris = [site + "/api/method/frappe.integrations.oauth2_logins.custom/bloomstack"]
+	redirect_uris = [site + redirect_uri]
 	scopes = ["openid", "roles", "email", "profile"]
 
 	url = base_url + "/client/v1/create"
@@ -31,25 +45,52 @@ def create_client():
 		"authenticationMethod": "PUBLIC_CLIENT"
 	}
 
-	result = requests.post(url, headers=headers, data=json.dumps(data))
+	response = requests.post(url, headers=headers, data=json.dumps(data))
 
-	"""
-	result = {
-		"name": "company_name",
-		"isTrusted": "0",
-		"autoApprove": true,
-		"redirectUris": [site + "/api/method/frappe.integrations.oauth2_logins.custom/bloomstack"],
-		"allowedScopes": ["openid", "roles", "email", "profile"],
-		"authenticationMethod": "PUBLIC_CLIENT",
-		"createdBy": "<userID",
-		"modifiedBy": "<userID",
-		"creation": "2019-10-24T13:18:09.959Z",
-		"modified": "2019-10-24T13:18:09.959Z",
-		"clientId": "<clientID>",
-		"clientSecret": "<clientSecret>"
-	}
-	"""
+	if not response.ok:
+		frappe.throw(_("There was a server error while trying to create the site."), exc=ClientRegistrationError)
 
-	# TODO:
-	# 1) Handle social login keys creation
-	# 2) Handle OAuth Client creation
+	client_info = json.loads(response.content)
+	return client_info
+
+
+def create_social_login_keys(client_info):
+	# create a login key for Bloomstack, enabled by default
+	scopes = " ".join(client_info.get("allowedScopes"))
+	response_type = "code"
+	auth_url_data = json.dumps(dict(scope=scopes, response_type=response_type))
+
+	bloomstack_social_login_key = frappe.new_doc("Social Login Key")
+	bloomstack_social_login_key.update({
+		"enable_social_login": True,
+		"provider_name": "Bloomstack",
+		"client_id": client_info.get("clientId"),
+		"client_secret": client_info.get("clientSecret"),
+		"base_url": frappe.local.conf.get("auth_server"),
+		"authorize_url": frappe.local.conf.get("social_login_authorize_url"),
+		"redirect_url": frappe.local.conf.get("oauth_login_redirect_uri"),
+		"access_token_url": frappe.local.conf.get("social_login_access_token_url"),
+		"api_endpoint": frappe.local.conf.get("social_login_api_endpoint"),
+		"auth_url_data": auth_url_data
+	})
+	bloomstack_social_login_key.insert()
+
+	# create a login key for Frappe, disabled by default
+	frappe_social_login_key = frappe.new_doc("Social Login Key")
+	frappe_social_login_key.update({
+		"enable_social_login": False,
+		"provider_name": "Frappe",
+		"client_id": None,
+		"client_secret": None,
+		"base_url": get_url(),
+		"authorize_url": "/api/method/frappe.integrations.oauth2.authorize",
+		"redirect_url": "/api/method/frappe.www.login.login_via_frappe",
+		"access_token_url": "/api/method/frappe.integrations.oauth2.get_token",
+		"api_endpoint": "/api/method/frappe.integrations.oauth2.openid_profile",
+		"auth_url_data": json.dumps({"scope": "openid", "response_type": "code"})
+	})
+	frappe_social_login_key.insert()
+
+
+def create_oauth_client(client_info):
+	pass
