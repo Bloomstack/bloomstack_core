b6daca7ceddef9e1fef831372976215275be9824
Diksha <dikshajadhav11.dj@gmail.com>
2019-11-06 18:18:16 +0530

    fix: refactor code

--- bloomstack_core/notifications.py
+++ bloomstack_core/notifications.py
@@ -4,8 +4,8 @@ import frappe
 import json
 
 def get_notification_config():
-    setting = frappe.get_single("Notification Badges Settings")
+    settings = frappe.get_single("Notification Badges Settings")
     notifications = {"for_doctype":{}}
-    for config in setting.as_dict().configuration:
+    for config in settings.configuration:
         notifications["for_doctype"][config.filter_doctype] = json.loads(config.filter)
     return notifications
