5045fef74f4ae73441267ab31aabca346af0bedf
Diksha <dikshajadhav11.dj@gmail.com>
2019-11-07 17:28:14 +0530

    feat: display invalid filter rows

--- bloomstack_core/bloomstack_core/doctype/notification_badges_settings/notification_badges_settings.py
+++ bloomstack_core/bloomstack_core/doctype/notification_badges_settings/notification_badges_settings.py
@@ -4,7 +4,15 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class NotificationBadgesSettings(Document):
-	pass
+	def validate(self):
+		invalid_filter_rows = []
+		for i in self.as_dict().configuration:
+			if not frappe.utils.html_utils.is_json(i.filter):
+				invalid_filter_rows.append(self.as_dict().configuration.index(i)+1)
+		if invalid_filter_rows:
+			invalid_rows = ",".join(str(e) for e in invalid_filter_rows)
+			frappe.throw(_("The json format for filter is invalid in row(s) " + invalid_rows))
--- bloomstack_core/notifications.py
+++ bloomstack_core/notifications.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 import frappe
 import json
 
+
 def get_notification_config():
-    settings = frappe.get_single("Notification Badges Settings")
-    notifications = {"for_doctype":{}}
-    for config in settings.configuration:
-        notifications["for_doctype"][config.filter_doctype] = json.loads(config.filter)
-    return notifications
+	notifications = {"for_doctype": {}}
+	if frappe.db.exists("Notification Badges Settings"):
+		settings = frappe.get_single("Notification Badges Settings")
+        for config in settings.configuration:
+            notifications["for_doctype"][config.filter_doctype] = json.loads(config.filter)
+	return notifications
