4b8c96fa35420ebd9b2b3a0cb1d5b405dbb53a52
Diksha <dikshajadhav11.dj@gmail.com>
2019-11-08 18:00:19 +0530

    fix: validate filters

--- bloomstack_core/bloomstack_core/doctype/notification_badges_settings/notification_badges_settings.py
+++ bloomstack_core/bloomstack_core/doctype/notification_badges_settings/notification_badges_settings.py
@@ -5,14 +5,31 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
+import json
 from frappe.model.document import Document
+
 
 class NotificationBadgesSettings(Document):
 	def validate(self):
+		if self.configuration:
+			for i in self.as_dict().configuration:
+				invalid_filter_rows = self.validate_filter_json(i)
+
+			if invalid_filter_rows:
+				invalid_rows = ",".join(str(e) for e in invalid_filter_rows)
+				frappe.throw(_("The json format for filter is invalid in row(s) " + invalid_rows))
+
+	def validate_filter_json(self, row):
 		invalid_filter_rows = []
-		for i in self.as_dict().configuration:
-			if not frappe.utils.html_utils.is_json(i.filter):
-				invalid_filter_rows.append(self.as_dict().configuration.index(i)+1)
-		if invalid_filter_rows:
-			invalid_rows = ",".join(str(e) for e in invalid_filter_rows)
-			frappe.throw(_("The json format for filter is invalid in row(s) " + invalid_rows))
+		if frappe.utils.html_utils.is_json(row.filter):
+			self.validate_filter_attributes(row)
+		else:
+			invalid_filter_rows.append(self.as_dict().configuration.index(row)+1)
+		return invalid_filter_rows
+
+	def validate_filter_attributes(self, row):
+		filter = json.loads(row.filter)
+		for key in filter.keys():
+			if not hasattr(frappe.get_meta(row.filter_doctype), key):
+				frappe.throw(_("The Filter in row '{0}' is invalid").format(
+					self.as_dict().configuration.index(row)+1))
--- bloomstack_core/notifications.py
+++ bloomstack_core/notifications.py
@@ -8,6 +8,6 @@ def get_notification_config():
 	notifications = {"for_doctype": {}}
 	if frappe.db.exists("Notification Badges Settings"):
 		settings = frappe.get_single("Notification Badges Settings")
-        for config in settings.configuration:
-            notifications["for_doctype"][config.filter_doctype] = json.loads(config.filter)
+		for config in settings.configuration:
+			notifications["for_doctype"][config.filter_doctype] = json.loads(config.filter)
 	return notifications
