a3262f7f33eb9b3d4b2a512fb009b93a903df6d2
DeeMysterio <dikshajadhav11.dj@gmail.com>
2019-11-15 18:36:15 +0530

    feat(Notification Badges): override the hardcoded filters via ui (#96)
    * feat(Notification Badges): override the hardcoded filters via ui
    
    * fix: handle complex filters
    
    * fix: refactor code
    
    * feat: display invalid filter rows
    
    * fix: validate filters
    
    * fix: validate filter JSON and attributes
    
    * fix: refactor code

---
+++ bloomstack_core/bloomstack_core/doctype/notification_badges_settings/notification_badges_settings.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Bloom Stack, Inc and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+import json
+from frappe.model.document import Document
+from frappe.utils.html_utils import is_json
+
+class NotificationBadgesSettings(Document):
+	def validate(self):
+		for config in self.configuration:
+			self.validate_filter_json(config)
+			self.validate_filter_attributes(config)
+
+	def validate_filter_json(self, row):
+		if not is_json(row.filter):
+			frappe.throw(_("Row {0}: The filter's JSON format is invalid".format(row.idx)))
+
+	def validate_filter_attributes(self, row):
+		notification_filter = json.loads(row.filter)
+		for attr in notification_filter:
+			if not hasattr(frappe.get_meta(row.filter_doctype), attr):
+				frappe.throw(_("Row {0}: '{1}' is not a valid attribute in {2}").format(row.idx, attr, row.filter_doctype))
