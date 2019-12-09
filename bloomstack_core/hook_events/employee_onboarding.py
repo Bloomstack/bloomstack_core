85851e444ac6ce1472385971b7d90379c0f48381
Diksha <dikshajadhav11.dj@gmail.com>
2019-12-09 15:06:55 +0530

    feat: have a status called not started

--- bloomstack_core/hook_events/employee_onboarding.py
+++ bloomstack_core/hook_events/employee_onboarding.py
@@ -5,9 +5,9 @@ from frappe import _
 
 
 def send_emails_to_assigned_users(employee_onboarding, method):
-
 	for activity in employee_onboarding.activities:
-		recipients = activity.user
-		subject = "An employee onboarding activity has been assigned to you."
-		message = "You've been assigned to the activity: {0}".format(str(activity.activity_name))
-		frappe.sendmail(recipients=recipients, subject=subject, message=message)
+		if activity.user:
+			recipients = activity.user
+			subject = "An employee onboarding activity has been assigned to you."
+			message = "You've been assigned to the activity: {0}".format(str(activity.activity_name))
+			frappe.sendmail(recipients=recipients, subject=subject, message=message)
