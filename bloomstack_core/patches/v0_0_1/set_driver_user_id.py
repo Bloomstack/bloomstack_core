6cc1331c53ae4ab05cab8fe8865349a2d3186ed8
Parth Kharwar <parth@digithinkit.com>
2019-12-10 13:13:20 +0530

    fix(patch): json reload command added

--- bloomstack_core/patches/v0_0_1/set_driver_user_id.py
+++ bloomstack_core/patches/v0_0_1/set_driver_user_id.py
@@ -1,11 +1,12 @@
 import frappe
+from frappe.modules.utils import sync_customizations
 
 
 def execute():
-    frappe.reload_doc("hr", "doctype", "driver")
+    sync_customizations("bloomstack_core")
     drivers = frappe.get_all("Driver", filters={"employee": ["!=", ""]}, fields=["name", "employee"])
-    
-    for driver in drivers: 
+
+    for driver in drivers:
         user_id = frappe.db.get_value("Employee", driver.employee, "user_id")
         if user_id:
             frappe.db.set_value("Driver", driver.name, "user_id", user_id, update_modified=False)
