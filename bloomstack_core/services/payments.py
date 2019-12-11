980a2a574796fba6993775498e1c1daafcaadf3b
Rohan Bansal <rohan@digithinkit.com>
2019-12-11 15:49:47 +0530

    fix: if no invoice found, backtrack and find one using delivery note

--- bloomstack_core/services/payments.py
+++ bloomstack_core/services/payments.py
@@ -4,16 +4,16 @@ from erpnext.stock.doctype.delivery_note.delivery_note import make_sales_return
 
 
 @frappe.whitelist()
-def make_payment(amount, delivery_note, sales_invoice, returned_items=None):
+def make_payment(amount, delivery_note, sales_invoice=None, returned_items=None):
 	"""
 	Make a Payment Entry for the received amount against a delivery.
 
 	If all the items are not delivered, then create a return Delivery Note against the returned items.
 
 	Args:
-		amount (float): The amount paid for the delivery
-		delivery_note (str): The reference Delivery Note that contains order info
-		sales_invoice (str): The reference Sales Invoice against which the payment is made
+		amount (float): The amount paid for the delivery.
+		delivery_note (str): The reference Delivery Note that contains order info.
+		sales_invoice (str, optional): The reference Sales Invoice against which the payment is made.
 		returned_items (list of dict, optional): Any delivered items that have been returned. Defaults to None.
 
 	Returns:
@@ -21,9 +21,43 @@ def make_payment(amount, delivery_note, sales_invoice, returned_items=None):
 	"""
 
 	# generate a payment entry for the delivered items
+	if not sales_invoice:
+		invoices = frappe.get_all("Delivery Note Item",
+			filters={"docstatus": 1, "parent": delivery_note},
+			fields=["distinct(against_sales_invoice)"])
+
+		if invoices:
+			sales_invoice = invoices[0].against_sales_invoice
+
+	if not sales_invoice:
+		return {"error": "No invoice found to make payment against"}
+
 	payment_id = make_payment_entry(amount, sales_invoice)
 
 	# generate a return delivery note, if applicable
+	return_delivery_id = None
+	if returned_items:
+		return_delivery_id = make_return_delivery(delivery_note, returned_items)
+
+	# return payment and return info
+	return {
+		"payment_id": payment_id,
+		"return_delivery_id": return_delivery_id
+	}
+
+
+def make_return_delivery(delivery_note, returned_items):
+	"""
+	If all the ordered items are not delivered, create a return Delivery Note against the returned items.
+
+	Args:
+		delivery_note (str): The reference Delivery Note that contains order info.
+		returned_items (list of dict): Any delivered items that have been returned.
+
+	Returns:
+		str: The generated draft return Delivery Note ID
+	"""
+
 	if isinstance(returned_items, list):
 		returned_items_map = {d.get("item_code"): d.get("qty") for d in returned_items}
 		return_delivery = make_sales_return(delivery_note)
@@ -34,10 +68,5 @@ def make_payment(amount, delivery_note, sales_invoice, returned_items=None):
 
 		return_delivery.save()
 		return_delivery_id = return_delivery.name
-	else:
-		return_delivery_id = None
 
-	return {
-		"payment_id": payment_id,
-		"return_delivery_id": return_delivery_id
-	}
+		return return_delivery_id
