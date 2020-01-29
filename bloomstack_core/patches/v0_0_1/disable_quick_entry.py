953f22246b5cd50d3264da406194d66b169046b0
Dharmraj <Dharmraj@bloomstack.com>
2020-01-29 15:39:16 +0530

    fix: updated quick entry by sql

--- bloomstack_core/patches/v0_0_1/disable_quick_entry.py
+++ bloomstack_core/patches/v0_0_1/disable_quick_entry.py
@@ -1,7 +1,9 @@
 import frappe
 
 def execute():
-    doctypes = frappe.db.get_all('DocType', filters={'quick_entry': 1}, fields=['name'])
-    
-    for doctype in doctypes: 
-        frappe.db.set_value('DocType', doctype.name, 'quick_entry', 0)
+    frappe.db.sql("""UPDATE 
+                `tabDocType` 
+            SET 
+                quick_entry = 0
+            WHERE 
+                quick_entry = 1""")
