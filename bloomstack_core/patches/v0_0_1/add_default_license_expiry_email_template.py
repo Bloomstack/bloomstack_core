86494aa7dcac491b296711b487ed5551d840b109
vishal <vishaldhayagude09@gmail.com>
2020-02-26 11:31:36 +0530

    fix: patch added

---
+++ bloomstack_core/patches/v0_0_1/add_default_license_expiry_email_template.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+import os
+
+import frappe
+from frappe import _
+
+
+def execute():
+	frappe.reload_doc("email", "doctype", "email_template")
+	frappe.reload_doc("Bloomstack Core", "doctype", "Compliance Settings")
+
+	if not frappe.db.exists("Email Template", _("License Expiry Alert")):
+		base_path = frappe.get_app_path("bloomstack_core", "templates", "emails")
+		response = frappe.read_file(os.path.join(base_path, "license_expiry_reminder.md"))
+
+		frappe.get_doc({
+			"doctype": "Email Template",
+			"name": _("License Expiry Alert"),
+			"response": response,
+			"subject": _("License Expiry Alert!"),
+			"owner": frappe.session.user,
+		}).insert(ignore_permissions=True)
+
+	compliance_settings = frappe.get_doc("Compliance Settings")
+    compliance_settings.license_expiry_email_template = _("License Expiry Alert")
+	compliance_settings.save()
