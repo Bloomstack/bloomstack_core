5f5fc1c33cc7b1bac451b39196dd1d136d6033cb
vishdha <vishaldhayagude07@gmail.com>
2020-04-03 17:13:46 +0530

    fix:typo

--- bloomstack_core/public/js/lead.js
+++ bloomstack_core/public/js/lead.js
@@ -3,14 +3,6 @@ frappe.ui.form.on("Lead", {
         frm.set_query("region", { "is_group": 1 });
     },
     onload: function(frm) {
-		// frm.set_query('territory', function(doc) {
-		// 	return {
-		// 		filters: {
-		// 			"is_group": 0,
-		// 			"parent_territory": doc.region
-		// 		}
-		// 	};
-		// });
 		frm.set_query("territory", () => {
 			if (frm.doc.region) {
 				return {
