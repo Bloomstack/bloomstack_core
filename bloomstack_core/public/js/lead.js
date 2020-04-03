87f7be5550eb121c82e44d3288e8556d053f5a65
vishdha <vishaldhayagude07@gmail.com>
2020-04-03 17:16:37 +0530

    fix: indentation

--- bloomstack_core/public/js/lead.js
+++ bloomstack_core/public/js/lead.js
@@ -1,8 +1,8 @@
 frappe.ui.form.on("Lead", {
 	setup: (frm) => {
-        frm.set_query("region", { "is_group": 1 });
-    },
-    onload: function(frm) {
+		frm.set_query("region", { "is_group": 1 });
+	},
+	onload: function(frm) {
 		frm.set_query("territory", () => {
 			if (frm.doc.region) {
 				return {
