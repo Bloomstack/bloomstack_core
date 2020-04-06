3f5d9d6fd61114d6fbda31636702439814a27579
Rohan Bansal <rohan@digithinkit.com>
2020-04-06 16:11:05 +0530

    fix: patch logic

--- bloomstack_core/hook_events/lead.py
+++ bloomstack_core/hook_events/lead.py
@@ -10,5 +10,9 @@ def filter_territory(doctype, txt, searchfield, start, page_len, filters):
 	territory_list.append(filters.get("region"))
 
 	return frappe.get_all('Territory',
-		filters = {'parent_territory': ('in', territory_list), 'territory_name': ("like", "{0}%".format(txt))},
-		fields = ["name"], as_list=1)
+		filters={
+			'parent_territory': ('in', territory_list),
+			'territory_name': ("like", "%{0}%".format(txt))
+		},
+		fields=["name"],
+		as_list=1)
--- bloomstack_core/patches/v0_0_1/account_opened_date.py
+++ bloomstack_core/patches/v0_0_1/account_opened_date.py
@@ -4,20 +4,14 @@ from frappe.modules.utils import sync_customizations
 
 def execute():
 	sync_customizations("bloomstack_core")
+
 	leads = frappe.get_all("Lead", fields=["name", "creation"])
-
 	for lead in leads:
-		result = frappe.db.get_value("Customer", {"lead_name": lead.name}, ["creation","opening_date"], as_dict=True)
-		if result.opening_date:
-			frappe.db.set_value("Lead", lead.name, "account_opened_date", result.opening_date)
-		else:
-			frappe.db.set_value("Lead", lead.name, "account_opened_date", result.creation)
+		result = frappe.db.get_value("Customer", {"lead_name": lead.name}, ["creation", "opening_date"], as_dict=True)
+		if result:
+			frappe.db.set_value("Lead", lead.name, "account_opened_date", result.opening_date or result.creation)
 
-
-	customers = frappe.get_all("Customer", fields=["opening_date", "lead_name"])
-
+	customers = frappe.get_all("Customer", filters={"lead_name": ("!=", "")}, fields=["opening_date", "lead_name"])
 	for customer in customers:
 		if customer.lead_name:
-			doc = frappe.get_doc("Lead", customer.lead_name)
-			doc.account_opening_date = customer.opening_date
-			doc.save()
+			frappe.db.set_value("Lead", customer.lead_name, "account_opened_date", customer.opening_date)
