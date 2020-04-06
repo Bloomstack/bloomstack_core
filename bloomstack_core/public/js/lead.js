b8452b1f363fe9d503737a8cdca5d4479964ac90
Rohan Bansal <rohan@digithinkit.com>
2020-04-06 19:33:40 +0530

    fix: handle empty response

--- bloomstack_core/public/js/lead.js
+++ bloomstack_core/public/js/lead.js
@@ -17,10 +17,12 @@ frappe.ui.form.on("Lead", {
 
 		if (!frm.doc.account_opened_date) {
 			frappe.db.get_value("Customer", { "lead_name": frm.doc.name }, ["opening_date", "creation"], (r) => {
-				if (r.opening_date) {
-					frm.set_value("account_opened_date", r.opening_date);
-				} else {
-					frm.set_value("account_opened_date", r.creation);
+				if (r) {
+					if (r.opening_date) {
+						frm.set_value("account_opened_date", r.opening_date);
+					} else {
+						frm.set_value("account_opened_date", r.creation);
+					}
 				}
 			});
 		}
