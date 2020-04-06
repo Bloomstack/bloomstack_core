caa4a405c4f9f67b5b3f1631a0c19e1943de2ccc
vishdha <vishaldhayagude07@gmail.com>
2020-04-06 14:33:44 +0530

    fix: filtering txt added and minor changes in lead added

--- bloomstack_core/hook_events/lead.py
+++ bloomstack_core/hook_events/lead.py
@@ -9,7 +9,6 @@ def filter_territory(doctype, txt, searchfield, start, page_len, filters):
 	territory_list = get_descendants_of("Territory", filters.get("region"))
 	territory_list.append(filters.get("region"))
 
-	return frappe.db.get_all("Territory",
-		filters=[dict(parent_territory=('in', territory_list))],
-		fields=['name'],
-		as_list=1)
+	return frappe.get_all('Territory',
+		filters = {'parent_territory': ('in', territory_list), 'territory_name': ("like", "{0}%".format(txt))},
+		fields = ["name"], as_list=1)
