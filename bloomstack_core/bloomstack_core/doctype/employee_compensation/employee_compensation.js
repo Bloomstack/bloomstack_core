e08fec5186cdd986a74ebc874d35a06b79e64d2f
vishdha <vishaldhayagude07@gmail.com>
2020-04-15 14:25:42 +0530

    fix: Date validation added

--- bloomstack_core/bloomstack_core/doctype/employee_compensation/employee_compensation.js
+++ bloomstack_core/bloomstack_core/doctype/employee_compensation/employee_compensation.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Compensation', {
-	// refresh: function(frm) {
-
-	// }
+	validate: function(frm) {
+		if (frm.doc.date_of_injury > frappe.datetime.get_today()) {
+			frappe.msgprint(__("You can not select future date in Date of Injury"));
+			frappe.validated = false;
+		}
+		if (frm.doc.first_aid_date > frappe.datetime.get_today()) {
+			frappe.msgprint(__("You can not select future date in first Aid"));
+			frappe.validated = false;
+		}
+		if (frm.doc.first_aid_date < frm.doc.date_of_injury) {
+			frappe.msgprint(__("You can not select first aid date less than Injury Date"));
+			frappe.validated = false;
+		}
+	}
 });
