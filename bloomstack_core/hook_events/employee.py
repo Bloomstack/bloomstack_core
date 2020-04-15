33122ac3ab7749126e9f4c56eba18801e472e018
Rohan Bansal <rohan@digithinkit.com>
2020-04-15 18:49:47 +0530

    fix: avoid hardcoded dashboard entry

--- bloomstack_core/hook_events/employee.py
+++ bloomstack_core/hook_events/employee.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
+
 import frappe
 from frappe import _
+
 
 def update_driver_employee(employee, method):
 	if method == "validate":
@@ -13,43 +15,10 @@ def update_driver_employee(employee, method):
 		if driver:
 			frappe.db.set_value("Driver", driver, "employee", employee.name if user_id else None)
 
+
 def get_data(data):
-	return frappe._dict({
-		'heatmap': True,
-		'heatmap_message': _('This is based on the attendance of this Employee'),
-		'fieldname': 'employee',
-		'transactions': [
-			{
-				'label': _('Leave and Attendance'),
-				'items': ['Attendance', 'Attendance Request', 'Leave Application', 'Leave Allocation', 'Employee Checkin']
-			},
-			{
-				'label': _('Lifecycle'),
-				'items': ['Employee Transfer', 'Employee Promotion', 'Employee Separation']
-			},
-			{
-				'label': _('Shift'),
-				'items': ['Shift Request', 'Shift Assignment']
-			},
-			{
-				'label': _('Expense'),
-				'items': ['Expense Claim', 'Travel Request', 'Employee Advance']
-			},
-			{
-				'label': _('Benefit'),
-				'items': ['Employee Benefit Application', 'Employee Benefit Claim' ,'Employee Compensation']
-			},
-			{
-				'label': _('Evaluation'),
-				'items': ['Appraisal']
-			},
-			{
-				'label': _('Payroll'),
-				'items': ['Salary Structure Assignment', 'Salary Slip', 'Additional Salary', 'Timesheet','Employee Incentive', 'Retention Bonus']
-			},
-			{
-				'label': _('Training'),
-				'items': ['Training Event', 'Training Result', 'Training Feedback', 'Employee Skill Map']
-			},
-		]
-	})
+	for transaction in data.transactions:
+		if transaction.get("label") == "Benefit":
+			transaction.get("items", []).append("Employee Compensation")
+
+	return data
