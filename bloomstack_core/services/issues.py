84a8abaaff4832d65c2f6dcc6884b806cf8c70ef
vishdha <vishaldhayagude07@gmail.com>
2020-04-27 16:21:39 +0530

    removed typo

--- bloomstack_core/services/issues.py
+++ bloomstack_core/services/issues.py
@@ -5,5 +5,5 @@ def issue_status_list():
 	"""
 	Returns an array of statuses available on the Issues doctype.
 	"""
-	meta = frappe.get_meta("Lead")
+	meta = frappe.get_meta("Issue")
 	return meta.get_options("status").split("\n")
