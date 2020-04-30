9c0e87858f05979921d3deba5dc5707597f79e11
vishdha <vishaldhayagude07@gmail.com>
2020-04-30 12:20:06 +0530

    fix: conversion factor logic

--- bloomstack_core/hook_events/utils.py
+++ bloomstack_core/hook_events/utils.py
@@ -2,6 +2,7 @@ import frappe
 from frappe import _
 from frappe.utils import getdate, nowdate
 from frappe.utils import cstr
+from erpnext.stock.doctype.item.item import get_uom_conv_factor
 
 
 def validate_license_expiry(doc, method):
@@ -26,7 +27,13 @@ def validate_entity_license(party_type, party_name):
 			frappe.bold(party_name), frappe.bold(license_number), frappe.bold(license_expiry_date)))
 
 def validate_cultivation_tax(doc, method):
-	if doc.doctype in ("Purchase Order", "Purchase Invoice", "Purchase Receipt"):
+	if doc.doctype in ("Purchase Order", "Purchase Invoice", "Purchase Receipt", "Sales Order", "Sales Invoice", "Delivery_note"):
+
+		# if customer is distributor then calculate cultivation tax.
+		customer_group = frappe.db.get_value("Customer", filters= {"name": doc.customer}, fieldname=['customer_group'])
+		if not customer_group == "Distributor":
+			return 
+
 		items = []
 		cultivated_tax = 0
 		for d in doc.get("items"):
@@ -38,7 +45,7 @@ def validate_cultivation_tax(doc, method):
 			if not cultivated_item.enable_cultivation_tax:
 				return
 			else:
-				ounce_qty = convert_ounce(d.stock_qty)
+				ounce_qty = convert_to_ounce(d.name, d.weight_uom, d.total_weight)
 				if cultivated_item.cultivation_tax_type == "Dry Flower":
 					cultivated_tax = cultivated_tax +  ounce_qty * 9.65
 				if cultivated_item.cultivation_tax_type == "Dry Leaf":
@@ -48,12 +55,29 @@ def validate_cultivation_tax(doc, method):
 				# calculate_cultivation_tax()
 
 			items.append(cstr(d.item_code))
+		print("=======================culivation_tax============================", cultivated_tax)
+
+		row = doc.append('taxes', {})
+		row.category = 'Total'
+		row.charge_type = "On Item Quantity"
+		row.add_deduct_tax = "Deduct"
+		row.description = "Cultivation Tax"
+		row.account_head = "Cultivation Tax - Dried Flower - HF"
+		row.tax_amount = cultivated_tax
+		row.total = doc.total - cultivated_tax
 
 # def check_cultivation_tax(item_code, docname):
 	# pass
-	
-def convert_ounce(qty):
-	"convert grams to ounce"
 
-	g_to_oz_value = 28.35
-	return qty/g_to_oz_value
+def validate_excise_tax(doc, method):
+	pass
+
+
+	
+def convert_to_ounce(item_code, uom, qty):
+	"convert any unit into ounce"
+	conversion_factor = get_uom_conv_factor(uom, 'Ounce')
+	if not conversion_factor:
+		frappe.throw(_("Add UOM Conversion Factor for {0} to Ounce").format(uom))
+	value = qty * conversion_factor
+	return value
