ff412e06b23d0e431a69bce3f3ce3ab78dffa90d
Rohan Bansal <rohan@digithinkit.com>
2020-05-19 17:30:00 +0530

    fix: handle batch conflicts

--- bloomstack_core/public/js/pick_list.js
+++ bloomstack_core/public/js/pick_list.js
@@ -4,8 +4,20 @@ frappe.ui.form.on('Pick List Item', {
 
 		if (row.package_tag) {
 			frappe.db.get_value("Batch", {"package_tag": row.package_tag}, "name", (r) => {
-				if (r.name) {
-					frappe.model.set_value(cdt, cdn, "batch_no", r.name);
+				if (r && r.name) {
+					if (row.batch_no) {
+						// check if a different batch already exists, and ask for confirmation before overriding
+						if (row.batch_no != r.name) {
+							frappe.confirm(__("The {0} tag is linked to a different batch ({1}). Setting this package tag will not override the batch.<br><br>Do you still want to continue?", [row.package_tag, r.name]),
+								() => {},
+								() => {
+									frappe.model.set_value(cdt, cdn, "package_tag", "");
+								}
+							);
+						}
+					} else {
+						frappe.model.set_value(cdt, cdn, "batch_no", r.name);
+					}
 				}
 			});
 		}
