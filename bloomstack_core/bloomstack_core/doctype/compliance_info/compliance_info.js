4f0b438966a79c0ff16253aeaff5fc6001ac0ed3
Rohan <Alchez@users.noreply.github.com>
2020-05-21 16:47:50 +0530

    fix: prevent creation of Customer and Supplier for expired licenses (#237)

--- bloomstack_core/bloomstack_core/doctype/compliance_info/compliance_info.js
+++ bloomstack_core/bloomstack_core/doctype/compliance_info/compliance_info.js
@@ -3,21 +3,22 @@
 
 frappe.ui.form.on('Compliance Info', {
 	refresh: (frm) => {
-		console.log(!frm.is_new());
 		if (!frm.is_new()) {
-			frm.add_custom_button(__("Customer"), () => {
-				frappe.model.open_mapped_doc({
-					method: "bloomstack_core.utils.create_customer",
-					frm: frm
-				});
-			}, __("Create"));
+			if (!frm.doc.license_expiry_date || frm.doc.license_expiry_date > frappe.datetime.get_today()) {
+				frm.add_custom_button(__("Customer"), () => {
+					frappe.model.open_mapped_doc({
+						method: "bloomstack_core.utils.create_customer",
+						frm: frm
+					});
+				}, __("Create"));
 
-			frm.add_custom_button(__("Supplier"), () => {
-				frappe.model.open_mapped_doc({
-					method: "bloomstack_core.utils.create_supplier",
-					frm: frm
-				});
-			}, __("Create"));
+				frm.add_custom_button(__("Supplier"), () => {
+					frappe.model.open_mapped_doc({
+						method: "bloomstack_core.utils.create_supplier",
+						frm: frm
+					});
+				}, __("Create"));
+			}
 		}
 	}
 });
