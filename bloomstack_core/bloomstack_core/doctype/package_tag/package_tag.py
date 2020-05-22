034f6e79488b7558a14ce2a860a8c25d8181c5fc
Rohan Bansal <rohan@digithinkit.com>
2020-05-22 14:47:19 +0530

    fix: add batch and source package tag in Package Tag

--- bloomstack_core/bloomstack_core/doctype/package_tag/package_tag.py
+++ bloomstack_core/bloomstack_core/doctype/package_tag/package_tag.py
@@ -3,8 +3,18 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
+
 class PackageTag(Document):
-	pass
+	def validate(self):
+		if self.source_package_tag:
+			self.validate_source_package_tag()
+
+	def validate_source_package_tag(self):
+		source_package_tag = frappe.db.get_value("Package Tag", self.source_package_tag, "source_package_tag")
+		if self.name == source_package_tag:
+			frappe.throw(_("Invalid package tag. {0} is already the source package for {1}.".format(self.name, self.source_package_tag)))
