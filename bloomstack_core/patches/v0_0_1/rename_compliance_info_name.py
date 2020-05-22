a66ee2799c73444d67893590da5c274d8a5b0313
vishdha <vishaldhayagude07@gmail.com>
2020-05-22 15:08:06 +0530

    fix: Reload Doc added for compliance info

--- bloomstack_core/patches/v0_0_1/rename_compliance_info_name.py
+++ bloomstack_core/patches/v0_0_1/rename_compliance_info_name.py
@@ -2,8 +2,7 @@ import frappe
 from frappe.modules.utils import sync_customizations
 
 def execute():
-	sync_customizations("bloomstack_core")
-
+	frappe.reload_doc('bloomstack_core', 'doctype', 'compliance_info', force=True)
 	compliance_info_list = frappe.get_all("Compliance Info", fields=["name", "license_number"])
 	
 	for compliance_info in compliance_info_list:
