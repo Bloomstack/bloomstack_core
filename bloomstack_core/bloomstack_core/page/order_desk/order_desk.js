464ff342454bff2a877da970cfc45ad538f776e2
Myuddin khatri <myuddin@digithinkit.com>
2020-05-27 14:17:23 +0530

    fix(order-desk): added event-listener to entire item row
    added event-listener to entire row

--- bloomstack_core/bloomstack_core/page/order_desk/order_desk.js
+++ bloomstack_core/bloomstack_core/page/order_desk/order_desk.js
@@ -1155,16 +1155,16 @@ class SalesOrderCart {
 				<div class="item-name list-item__content list-item__content--flex-1.5 ellipsis" data-item-code="${item.item_code}">
 					${item.item_name}
 				</div>
-				<div class="quantity list-item__content text-right">
+				<div class="quantity list-item__content text-right" data-item-code="${item.item_code}">
 					${get_quantity_html(item.qty)}
 				</div>
-				<div class="discount list-item__content text-right">
+				<div class="discount list-item__content text-right" data-item-code="${item.item_code}">
 					${item.discount_percentage}%
 				</div>
-				<div class="rate list-item__content text-right">
+				<div class="rate list-item__content text-right" data-item-code="${item.item_code}">
 					${get_rate_html(item.rate)}
 				</div>
-				<div class="action list-item__content text-right action_button">
+				<div class="action list-item__content text-right action_button" data-item-code="${item.item_code}">
 					<a class="btn btn-danger btn-xs" title="Delete" data-name="${item.item_name}" data-item-code="${item.item_code}">X</a>
 				</div>
 			</div>
