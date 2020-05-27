8276683bb61edbcb4aac818be86766d0328f4040
Neha Sacher <nehasacher143@gmail.com>
2020-05-27 13:35:35 +0530

    fix: added error handling

--- bloomstack_core/public/js/employee.js
+++ bloomstack_core/public/js/employee.js
@@ -1,14 +1,29 @@
+let employee_name;
 frappe.form.link_formatters['Employee'] = function(value, doc) {
-  if (!doc) {
-    return value
+  if (!value) {
+    frappe.msgprint({
+      title: __('Error'),
+      indicator: 'red',
+      message: __('Please enter a valid Employee Code')
+  });
   }
-  if (doc.employee_name) {
-    return doc.employee_name;
-  } 
   else {
-    frappe.db.get_value("Employee", { "name" : value }, "employee_name", (r) => {
-      employee_name = r.employee_name;
-    });
-    return employee_name;
+    if (!doc) {
+      return value
+    }
+    if (doc.employee_name) {
+      return doc.employee_name;
+    } 
+    else {
+      frappe.db.get_value("Employee", { "name" : value }, "employee_name", (r) => {
+        if(r.employee_name == '') {
+          employee_name = value
+        }
+        else {
+          employee_name = r.employee_name;
+        }
+      });
+      return employee_name;
+    }
   }
 }
