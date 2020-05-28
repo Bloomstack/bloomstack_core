a83da194e782e4ee1f05d0927de439d5748db793
Neha Sacher <nehasacher143@gmail.com>
2020-05-28 15:55:00 +0530

    feat: batch number is added to cart in order desk

--- bloomstack_core/bloomstack_core/page/order_desk/order_desk.js
+++ bloomstack_core/bloomstack_core/page/order_desk/order_desk.js
@@ -843,6 +843,7 @@ class SalesOrderCart {
 							<div class="list-item__content text-muted text-right">${__('Quantity')}</div>
 							<div class="list-item__content text-muted text-right">${__('Discount')}</div>
 							<div class="list-item__content text-muted text-right">${__('Rate')}</div>
+							<div class="list-item__content text-muted text-right">${__('Batch No.')}</div>
 							<div class="list-item__content text-muted text-right actions"></div>
 						</div>
 						<div class="cart-items">
@@ -1160,11 +1161,14 @@ class SalesOrderCart {
 				<div class="quantity list-item__content text-right" data-item-code="${item.item_code}">
 					${get_quantity_html(item.qty)}
 				</div>
-				<div class="discount list-item__content text-right" data-item-code="${item.item_code}">
+				<div class="discount list-item__content text-center" data-item-code="${item.item_code}">
 					${item.discount_percentage}%
 				</div>
 				<div class="rate list-item__content text-right" data-item-code="${item.item_code}">
 					${get_rate_html(item.rate)}
+				</div>
+				<div class="batch list-item__content text-right" data-item-code="${item.item_code}">
+					${get_batch_no(item.batch_no)}
 				</div>
 				<div class="action list-item__content text-right action_button" data-item-code="${item.item_code}">
 					<a class="btn btn-danger btn-xs" title="Delete" data-name="${item.item_name}" data-item-code="${item.item_code}">X</a>
@@ -1188,6 +1192,15 @@ class SalesOrderCart {
 			`;
 		};
 
+		function get_batch_no(batch_no) {
+			if(batch_no != null) {
+				return batch_no
+			}
+			else {
+				return '-';
+			}
+		}
+
 		function get_rate_html(rate){
 			return `
 			<div class="input-group input-group-xs">
@@ -1203,6 +1216,8 @@ class SalesOrderCart {
 			</div>`
 		}
 	}
+
+	
 
 	get_item_details(item_code) {
 		if (!this.item_data[item_code]) {
