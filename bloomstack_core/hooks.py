7e6b67460a27917b641063657621588a1bad7656
Neil Trini Lasrado <neil@digithinkit.com>
2020-05-29 19:29:00 +0530

    [chore] Renamed update_bloomstack_customer_user function to update_bloomstack_site_user (#267)

--- bloomstack_core/hooks.py
+++ bloomstack_core/hooks.py
@@ -233,7 +233,7 @@ scheduler_events = {
 		"bloomstack_core.hook_events.sales_order.create_sales_invoice_against_contract"
 	],
 	"hourly": [
-		"bloomstack_core.bloomtrace.user.update_bloomstack_customer_user"
+		"bloomstack_core.bloomtrace.user.update_bloomstack_site_user"
 	]
 }
 
