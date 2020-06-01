45ae7ac23110c9171823b5b14229dfc249729d86
vishdha <vishaldhayagude07@gmail.com>
2020-06-01 13:46:49 +0530

    fix: minor changes added

--- bloomstack_core/public/js/project.js
+++ bloomstack_core/public/js/project.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on("Project", {
 	project_template: (frm) => {
-		if (frm.is_new && frm.doc.project_template) {
+		if (frm.is_new() && frm.doc.project_template) {
 			if (frm.doc.billable == 0) {
 				frappe.db.get_value("Project Template", { "name": frm.doc.project_template }, "billable", (r) => {
 					if (r && r.billable == 1) {
