12991e42b123f06d30562b8fc627f1331bf06f50
vishdha <vishaldhayagude07@gmail.com>
2020-06-02 17:50:05 +0530

    fix: move all billable logic to utils.js

--- bloomstack_core/hooks.py
+++ bloomstack_core/hooks.py
@@ -75,8 +75,6 @@ doctype_js = {
 	"Lead": "public/js/lead.js",
 	"Packing Slip": "public/js/packing_slip.js",
 	"Pick List": "public/js/pick_list.js",
-	"Project Template": "public/js/project_template.js",
-	"Project Type": "public/js/project_type.js",
 	"Project": "public/js/project.js",
 	"Quality Inspection": "public/js/quality_inspection.js",
 	"Quotation": "public/js/quotation.js",
--- bloomstack_core/public/js/task.js
+++ bloomstack_core/public/js/task.js
@@ -5,59 +5,5 @@ frappe.ui.form.on("Task", {
 				frm.set_value("billable", 1)
 			}
 		});
-	},
-	billable: (frm) => {
-		let confirm_message = ""
-		frappe.run_serially([
-			() => {
-				frappe.call({
-					method: "bloomstack_core.hook_events.utils.get_linked_documents",
-					args: {
-						doctype: frm.doc.doctype,
-						name: frm.doc.name
-					},
-					freeze:true,
-					callback: (r) => {
-						if (!r.exc && r.message.count > 0) {
-							// add confirmation message for cancelling all linked docs
-							let links_text = "";
-							let links = r.message.docs;
-							const doctypes = Array.from(new Set(links.map(link => link.doctype)));
-
-							for (let doctype of doctypes) {
-								let docnames = links
-									.filter((link) => link.doctype == doctype)
-									.map((link) => frappe.utils.get_form_link(link.doctype, link.name, true))
-									.join(", ");
-								links_text += `<li><strong>${doctype}</strong>: ${docnames}</li>`
-
-							}
-							links_text = "<ul>" + links_text + "</ul>"
-
-							confirm_message = `This <strong>${frm.doc.doctype}</strong> ${frm.doc.name} is linked with the following documents: ${links_text}`
-							frappe.confirm(__(`${confirm_message} Do you want to set them as {0} too?`, [frm.doc.billable ? "billed" : "unbilled"]),
-								() => {
-									frappe.call({
-										method: "bloomstack_core.hook_events.utils.update_timesheet_logs",
-										args: {
-											ref_dt: frm.doctype,
-											ref_dn: frm.doc.name,
-											billable: frm.doc.billable
-										},
-										callback: (r) => {
-											frm.save();
-										}
-									})
-								},
-								() => {
-									frm.doc.billable = !frm.doc.billable;
-									refresh_field("billable");
-									}
-							);
-						}
-					}
-				});
-			},
-		]);
 	}
 });
