3756e6aa3691412f43c1559e155c98da1f1a1d01
Neil Lasrado <neil@digithinkit.com>
2020-06-04 23:55:28 +0530

    Make an integration request to bloomtrace when a package tag is added or modified

--- bloomstack_core/bloomstack_core/doctype/package_tag/package_tag.py
+++ bloomstack_core/bloomstack_core/doctype/package_tag/package_tag.py
@@ -13,8 +13,24 @@ class PackageTag(Document):
 	def validate(self):
 		if self.source_package_tag:
 			self.validate_source_package_tag()
+		self.make_bloomtrace_integration_request()
+	
+	def after_insert(self):
+		self.make_bloomtrace_integration_request()
 
 	def validate_source_package_tag(self):
 		source_package_tag = frappe.db.get_value("Package Tag", self.source_package_tag, "source_package_tag")
 		if self.name == source_package_tag:
 			frappe.throw(_("Invalid package tag. {0} is already the source package for {1}.".format(self.name, self.source_package_tag)))
+
+	def make_bloomtrace_integration_request(self):
+		if frappe.get_conf().enable_bloomtrace and not self.is_new():
+			integration_request = frappe.new_doc("Integration Request")
+			integration_request.update({
+				"integration_type": "Remote",
+				"integration_request_service": "BloomTrace",
+				"status": "Queued",
+				"reference_doctype": "Package Tag",
+				"reference_docname": self.name
+			})
+			integration_request.save(ignore_permissions=True)
