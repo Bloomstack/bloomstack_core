11409b0571f78d57ddafebcfa4435e53c4ac5d0e
Rohan Bansal <rohan@digithinkit.com>
2020-06-09 22:41:30 +0530

    fix: hotpatch pick list function override

--- bloomstack_core/session.py
+++ bloomstack_core/session.py
@@ -1,6 +1,7 @@
 
 
 import frappe
+import erpnext.stock.doctype.pick_list.pick_list as pick_list
 
 
 def before_pick_list_save(self):
@@ -12,5 +13,4 @@ def override_pick_list_validation(*args, **kwargs):
 	# re-calculating the item quantities based on batch locations
 	# TODO: only keep until proper fix is made
 
-	from erpnext.stock.doctype.pick_list.pick_list import PickList
-	PickList.before_save = before_pick_list_save
+	pick_list.PickList.before_save = before_pick_list_save
