720b40a9ea04039b3abe0861e770f12d69e05ca1
Myuddin khatri <myuddin@digithinkit.com>
2020-06-16 16:51:00 +0530

    fix(purchase-receipt): fixed minor mistakes
    fixed minor mistakes

--- bloomstack_core/hook_events/purchase_receipt.py
+++ bloomstack_core/hook_events/purchase_receipt.py
@@ -13,21 +13,20 @@ def update_package_tags(pr, method):
 				frappe.db.set_value("Package Tag", item.package_tag, "batch_no", None)
 
 def create_package_tag(pr, method):
-	for item in pr.items:
-		package_tags = [item.package_tag for item in pr.items if item.package_tag]
+	package_tags = [item.package_tag for item in pr.items if item.package_tag]
 
-	if len(package_tags) == len(set(package_tags)):
-		for item in pr.items:
-			if item.package_tag:
-				package_tag = frappe.db.exists("Package Tag", {"package_tag": item.package_tag})
-				if package_tag:
-					frappe.throw("Package Tag already Exist")
-				else:
-					doc = frappe.new_doc("Package Tag")
-					doc.update({
-						"package_tag": item.package_tag,
-						"item_code": item.item_code
-					})
-					doc.save()
 	if len(package_tags) != len(set(package_tags)):
 		frappe.throw("Package Tag cannot be same for multiple Purchase Receipt Item")
+
+	for item in pr.items:
+		if item.package_tag:
+			package_tag = frappe.db.exists("Package Tag", {"package_tag": item.package_tag})
+			if package_tag:
+				frappe.throw("Package Tag already Exist")
+			else:
+				doc = frappe.new_doc("Package Tag")
+				doc.update({
+					"package_tag": item.package_tag,
+					"item_code": item.item_code
+				})
+				doc.save()
