6813bc36a9c29c871d7df497f4f9c3d73daccfb9
Rohan Bansal <rohan@digithinkit.com>
2020-06-18 17:04:33 +0530

    fix: create METRC packages from Delivery Notes

--- bloomstack_core/compliance/package.py
+++ bloomstack_core/compliance/package.py
@@ -3,7 +3,7 @@ from bloomstack_core.utils import get_metrc, log_request
 from frappe import _
 
 
-def create_package(stock_entry, method):
+def create_package_from_stock(stock_entry, method):
 	# TODO: Handle non-manufacture Stock Entries for intermediate packages
 	if stock_entry.stock_entry_type != "Manufacture":
 		return
@@ -13,7 +13,7 @@ def create_package(stock_entry, method):
 	if not metrc:
 		return
 
-	payload = build_payload(stock_entry)
+	payload = build_stock_payload(stock_entry)
 	response = metrc.packages.create.post(json=payload)
 	log_request(response.url, payload, response, "Stock Entry", stock_entry.name)
 
@@ -21,15 +21,15 @@ def create_package(stock_entry, method):
 		frappe.throw(_(response.raise_for_status()))
 
 
-def adjust_package(stock_entry, method):
+def adjust_package_from_stock(stock_entry, method):
 	# TODO: Handle non-manufacture Stock Entries for intermediate packages
 	if stock_entry.stock_entry_type != "Manufacture":
 		return
 
 
-def build_payload(stock_entry):
+def build_stock_payload(stock_entry):
 	"""
-	Create the request body for the Item endpoint.
+	Create the request body for package generation from a Stock Entry.
 
 	Args:
 		stock_entry (object): The `Stock Entry` Frappe object.
@@ -59,6 +59,71 @@ def build_payload(stock_entry):
 				"UnitOfMeasure": item.uom,
 				"PatientLicenseNumber": "",
 				"ActualDate": stock_entry.posting_date,
+			}
+
+	if not payload:
+		return
+
+	payload["Ingredients"] = package_ingredients
+	payload = [payload]
+	return payload
+
+
+def create_package_from_delivery(delivery_note, method):
+	if delivery_note.is_return:
+		return
+
+	metrc = get_metrc()
+
+	if not metrc:
+		return
+
+	for item in delivery_note.items:
+		if not frappe.db.exists("Compliance Item", item.item_code):
+			continue
+
+		if not item.package_tag:
+			continue
+
+		payload = build_delivery_payload(delivery_note, item)
+		response = metrc.packages.create.post(json=payload)
+		log_request(response.url, payload, response, "Delivery Note Item", item.name)
+
+		if not response.ok:
+			frappe.throw(_(response.raise_for_status()))
+
+
+def build_delivery_payload(delivery_note, item):
+	"""
+	Create the request body for package generation from a Delivery Note.
+
+	Args:
+		delivery_note (object): The `Delivery Note` Frappe object.
+		item (object): The `Delivery Note Item` Frappe object.
+
+	Returns:
+		payload (list of dict): The `Delivery Note` payload, if a Compliance Item is moved / created, otherwise `None`.
+	"""
+
+	payload = {}
+	package_ingredients = []
+
+	if item.package_tag:
+		source_package_tag = frappe.db.get_value("Package Tag", item.package_tag, "source_package_tag")
+		if source_package_tag:
+			package_ingredients.append({
+				"Package": source_package_tag,
+				"Quantity": item.qty,
+				"UnitOfMeasure": item.uom,
+			})
+		elif item.warehouse:
+			payload = {
+				"Tag": item.package_tag,
+				"Item": item.item_name,
+				"Quantity": item.qty,
+				"UnitOfMeasure": item.uom,
+				"PatientLicenseNumber": "",
+				"ActualDate": delivery_note.lr_date or delivery_note.posting_date
 			}
 
 	if not payload:
