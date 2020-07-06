2a2bf72793b333f43c779036c0537e1ad08b9119
vishdha <vishaldhayagude07@gmail.com>
2020-07-06 15:48:37 +0530

    fix:serverside check added

--- bloomstack_core/hook_events/production_plan.py
+++ bloomstack_core/hook_events/production_plan.py
@@ -2,16 +2,13 @@
 # Copyright (c) 2020, Bloomstack Inc. and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
-
 import frappe
-from frappe.utils import get_url, cstr
-from urllib.parse import urlparse
 
 def set_workstations(doc, method):
-    boms = frappe.get_all("BOM Operation" , fields=["*"])
-    for bom in doc.get("po_items"):
-        operations =  next((data for data in boms if data.get("parent") == bom.get("bom_no")), None)
-        if not operations:
-            continue
-        bom.workstations = operations.workstation
+	boms = frappe.get_all("BOM Operation" , fields=["*"])
+	workstation_value = []
+	for bom in doc.get("po_items"):
+		for operation in boms:
+			if operation.parent == bom.bom_no:
+			   workstation_value.append(operation.workstation)
+		bom.workstations = ', '.join(workstation_value)
--- bloomstack_core/public/js/production_plan.js
+++ bloomstack_core/public/js/production_plan.js
@@ -1,13 +1,13 @@
 frappe.ui.form.on("Production Plan Item", "bom_no", function(frm, cdt, cdn) {
 	let row = locals[cdt][cdn];
 	if (row.bom_no) {
+		let workstations_value = []
 		frappe.model.with_doc("BOM", row.bom_no, function() {
 			let bom_doc = frappe.model.get_doc("BOM", row.bom_no);
-			let workstations_value = '';
 			$.each(bom_doc.operations, function(i, s) {
-				workstations_value += s.workstation + ", "
+				workstations_value.push(s.workstation)
 			});
-			row.workstations = workstations_value;
+			row.workstations = workstations_value.join(", ")
 			frm.refresh_field("po_items");
 		});
 	}
