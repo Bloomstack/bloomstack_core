453dd76bc38d3ae93796d23a1f2fdcb459facd39
Myuddin khatri <myuddin@digithinkit.com>
2020-07-06 15:02:20 +0530

    feat(batch):added field item_group in Doctype Batch
    added field item_group in Doctype Batch and Item Group is showing in list view for Batch Doctype

---
+++ bloomstack_core/patches/v0_0_1/set_item_group_in_batch.py
@@ -0,0 +1,11 @@
+import frappe
+from frappe.modules.utils import sync_customizations
+
+
+def execute():
+	sync_customizations("bloomstack_core")
+	batches = frappe.get_all("Batch" , fields=["name", "item"])
+
+	for batch in batches:
+		item_group = frappe.db.get_value("Item", batch.item, "item_group")
+		frappe.db.set_value("Batch", batch, "item_group", item_group, update_modified=False)
