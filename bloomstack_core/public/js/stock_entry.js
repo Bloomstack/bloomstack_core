6d8209268959fcc65fe401453ded6977894a8b8f
Myuddin khatri <myuddin@digithinkit.com>
2020-07-08 16:44:07 +0530

    feat(warehouse): fixed problems
    fixed problems

--- bloomstack_core/public/js/stock_entry.js
+++ bloomstack_core/public/js/stock_entry.js
@@ -1,25 +1,43 @@
 frappe.ui.form.on("Stock Entry Detail", {
 	s_warehouse: function(frm, cdt, cdn){
-		var row = locals[cdt][cdn];
+		const row = locals[cdt][cdn];
 
-		frappe.db.get_value("Warehouse", {"name" : row.s_warehouse}, ["customer", "supplier"], (r) => {
-			if(r){
-				frappe.model.set_value(cdt, cdn, "customer", r.customer);
-				frappe.model.set_value(cdt, cdn, "supplier", r.supplier);
-				refresh_field("items");
+		if(row.s_warehouse){
+			frappe.db.get_value("Warehouse", {"name" : row.s_warehouse}, ["customer", "supplier"], (r) => {
+					frappe.model.set_value(cdt, cdn, "customer", r.customer || row.customer);
+					frappe.model.set_value(cdt, cdn, "supplier", r.supplier || row.supplier);
+					refresh_field("items");
+			})
+		}
+		else {
+			if(row.t_warehouse){
+				frm.trigger('t_warehouse')
 			}
-		})
+			else {
+				frappe.model.set_value(cdt, cdn, "customer", "");
+				frappe.model.set_value(cdt, cdn, "supplier", "");
+			}
+		}
 	},
 
 	t_warehouse: function(frm, cdt, cdn){
-		var row = locals[cdt][cdn];
+		const row = locals[cdt][cdn];
 
-		frappe.db.get_value("Warehouse", {"name" : row.t_warehouse}, ["customer", "supplier"], (r) => {
-			if(r){
-				frappe.model.set_value(cdt, cdn, "customer", r.customer);
-				frappe.model.set_value(cdt, cdn, "supplier", r.supplier);
-				refresh_field("items");
+		if(row.t_warehouse){
+			frappe.db.get_value("Warehouse", {"name" : row.t_warehouse}, ["customer", "supplier"], (r) => {
+					frappe.model.set_value(cdt, cdn, "customer", r.customer || row.customer);
+					frappe.model.set_value(cdt, cdn, "supplier", r.supplier || row.supplier);
+					refresh_field("items");
+			})
+		}
+		else {
+			if(row.s_warehouse){
+				frm.trigger("s_warehouse");
 			}
-		})
+			else {
+				frappe.model.set_value(cdt, cdn, "customer", "");
+				frappe.model.set_value(cdt, cdn, "supplier", "");
+			}
+		}
 	}
 })
