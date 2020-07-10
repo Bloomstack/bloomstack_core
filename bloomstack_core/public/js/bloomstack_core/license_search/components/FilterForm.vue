b86befeb2b1ed619676e2a5f8cf4256c32730caa
Neha Sacher <nehasacher143@gmail.com>
2020-07-10 17:37:50 +0530

    feat: mobile and desktop designs added

--- bloomstack_core/public/js/bloomstack_core/license_search/components/FilterForm.vue
+++ bloomstack_core/public/js/bloomstack_core/license_search/components/FilterForm.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="license-filter">
+    <div class="license-filter row">
         <form @submit.prevent="applyFilters">
-            <Input type="text" placeholder="Search" v-model="filters.search" />
-            <Input type="text" placeholder="Zipcode" v-model="filters.zip" />
-            <Select label="License Type" :options="license.licenseTypes" v-model="filters.licenseType" />
-
-            <input type="submit" value="Submit" />
+            <Input type="text" placeholder="Search" v-model="filters.search" class="col-md-6 col-sm-12 col-xs-12 search_box"/>
+            <Input type="text" placeholder="Zipcode" v-model="filters.zip" class="col-md-2 col-sm-4 col-xs-4 zip_box"/>
+            <Select placeholder="Type of License" :options="license.licenseTypes" v-model="filters.licenseType" class="col-md-3 col-sm-4 col-xs-4 type_select"/>
+            <div class="col-md-1 col-sm-4 col-xs-4">
+                <input type="submit" value="Search"/>
+            </div>
         </form>
     </div>
 </template>
--- bloomstack_core/public/js/bloomstack_core/license_search/components/License.vue
+++ bloomstack_core/public/js/bloomstack_core/license_search/components/License.vue
@@ -1,15 +1,25 @@
 <template>
-    <div class="license-card">
-        <div class="">
-
-        </div>
-        <div class="">
-            <div class="">
-                <h4>{{ license.legal_name }}</h4>
-                <p>{{ license.license_number }}</p>
-                <p>{{ license.license_type }}</p>
+    <div class="license-card row">
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="map">
+              <a href="">
+                <img src="assets/bloomstack_core/images/map.png" alt="">
+                <span>view map</span>
+              </a>
             </div>
-            <div class="">
+            <div class="actions">
+                <p><a href="" class="conversion_actions">Convert to lead</a></p>
+                <p><a href="" class="conversion_actions">Convert to customer</a></p>
+                <p><a href="" class="conversion_actions">Convert to supplier</a></p>
+            </div>
+        </div>
+        <div class="col-md-9 col-sm-9 col-xs-9">
+            <div class="row license_info">
+                <div class="col-md-4 col-sm-6 col-xs-6 legal_name">{{ license.legal_name }}</div>
+                <div class="col-md-4 col-sm-6 col-xs-6 license_number">{{ license.license_number }}</div>
+                <div class="col-md-4 col-sm-12 col-xs-12 license_type">{{ license.license_type }}</div>
+            </div>
+            <div class="row license_info">
                 <p>{{ license.zip_code }}|{{ license.county }}|{{ license.city }}</p>
                 <p>{{ license.email_id }}</p>
                 <p>License expiry: {{ license.expiration_date }}</p>
@@ -24,6 +34,9 @@
             license: Object
         }
     }
+    $('license-card').click(function() {
+        $('license-card .actions').toggleClass("show");
+    });
 </script>
 
 <style>
