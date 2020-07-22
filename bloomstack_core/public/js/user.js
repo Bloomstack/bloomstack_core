5ded0ab866e8ae0449f84628f71b0c1bf53863f4
Neil Lasrado <neil@digithinkit.com>
2020-07-22 22:17:58 +0530

    [fix] Remove code that enabled users to login as other users

--- bloomstack_core/public/js/user.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on("User", {
-	refresh: (frm) => {
-		bloomstack_core.add_login_as_button(frm,
-			__("Login As ") + frm.doc.first_name + (frm.doc.last_name ? " " + frm.doc.last_name : ""),
-			frm.doc.name);
-	}
-});
