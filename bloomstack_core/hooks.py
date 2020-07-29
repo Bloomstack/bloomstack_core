a338e64aa280b90e8c08e015dc157bf3780abc1c
vishdha <vishaldhayagude07@gmail.com>
2020-07-29 16:04:18 +0530

    fix: Remove all billable-project related logic

--- bloomstack_core/hooks.py
+++ bloomstack_core/hooks.py
@@ -82,13 +82,10 @@ doctype_js = {
 	"Lead": "public/js/lead.js",
 	"Packing Slip": "public/js/packing_slip.js",
 	"Pick List": "public/js/pick_list.js",
-	"Project": "public/js/project.js",
 	"Quality Inspection": "public/js/quality_inspection.js",
 	"Quotation": "public/js/quotation.js",
 	"Sales Order": "public/js/sales_order.js",
 	"Stock Entry": "public/js/stock_entry.js",
-	"Task": "public/js/task.js",
-	"Timesheet": "public/js/timesheet.js",
 	"Work Order": "public/js/work_order.js",
 	"Production Plan": "public/js/production_plan.js"
 }
