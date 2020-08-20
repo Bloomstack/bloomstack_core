044c12aa8698c7bf5c0470cd7b1693279c9477cf
Neha Sacher <nehasacher143@gmail.com>
2020-08-20 14:46:25 +0530

    feat: progress bar library added

--- bloomstack_core/public/js/bloomstack_core/license_search/Home.vue
+++ bloomstack_core/public/js/bloomstack_core/license_search/Home.vue
@@ -1,13 +1,18 @@
 <template>
+
     <div class="wrapper">
         <FilterForm />
         <LicenseList />
     </div>
+
 </template>
 
 <script>
     import FilterForm from './components/FilterForm.vue';
     import LicenseList from './components/LicenseList.vue';
+    import 'nprogress/nprogress.css';
+    import NProgress from 'nprogress';
+
     export default {
         components: {
             FilterForm,
