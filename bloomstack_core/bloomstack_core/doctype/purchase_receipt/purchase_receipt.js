8cda6ac39a70b880fd29fb85d83ef8e21cabae73
meshramsaroj <saroj@digithinit.com>
2020-08-31 13:47:27 +0530

    feat: Added section for Payment Terms in Puchase Receipt

---
+++ bloomstack_core/bloomstack_core/doctype/purchase_receipt/purchase_receipt.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Bloom Stack, Inc and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Purchase Receipt', {
+	// refresh: function(frm) {
+
+	// }
+});
