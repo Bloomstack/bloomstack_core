6d1efb170a121cd272cfbf47ff9fd0bc9f23eac1
Himanshu <himanshu@pop-os.localdomain>
2020-09-18 16:24:14 +0530

    feat: add setting for fetching transfers

--- bloomstack_core/hook_events/compliance_settings.py
+++ bloomstack_core/hook_events/compliance_settings.py
@@ -1,8 +1,7 @@
-from urllib.parse import urlparse
 
 import frappe
 from bloomstack_core.bloomtrace import get_bloomtrace_client
-from frappe.utils import get_url
+from frappe.utils import get_host_name
 
 
 def sync_bloomtrace(compliance_settings, method):
@@ -13,12 +12,13 @@ def sync_bloomtrace(compliance_settings, method):
 	if not frappe_client:
 		return
 
-	site_url = urlparse(get_url()).netloc
+	site_url = get_host_name()
 	frappe_client.update({
 		"doctype": "Bloomstack Site",
 		"name": site_url,
 		"metrc_url": compliance_settings.metrc_url,
 		"metrc_user_key": compliance_settings.get_password("metrc_user_key"),
 		"metrc_push_data": compliance_settings.metrc_push_data,
-		"metrc_pull_data": compliance_settings.metrc_pull_data
+		"metrc_pull_data": compliance_settings.metrc_pull_data,
+		"pull_incoming_transfer": compliance_settings.pull_incoming_transfer
 	})
