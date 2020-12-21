7a281a95b5093ec9f32d79da17bb57e004060cd7
nmimsnikhil <nikhilbachhawat.nmims@gmail.com>
2020-12-21 13:31:40 +0530

    fix: only deleting unwanted property setter

--- bloomstack_core/patches/v0_0_1/delete_bom_from_property_setter.py
+++ bloomstack_core/patches/v0_0_1/delete_bom_from_property_setter.py
@@ -4,4 +4,4 @@ def execute():
     # Delete BOM property from property setter.
     frappe.db.sql("""DELETE FROM `tabProperty Setter`
     WHERE
-    doc_type='BOM'""")
+    doc_type='BOM' AND property='title_field'""")
