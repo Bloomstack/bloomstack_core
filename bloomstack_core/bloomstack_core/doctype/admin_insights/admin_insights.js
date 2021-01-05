f60d0f21f00fe68ab1d2472907f525e8beec1938
bhattdevarsh <bhatt.devarsh17@gmail.com>
2021-01-05 19:55:17 +0530

    feat: Created Access Points for Dev Console to read write cube data

---
+++ bloomstack_core/bloomstack_core/doctype/admin_insights/admin_insights.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Bloom Stack, Inc and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Admin Insights', {
+	// refresh: function(frm) {
+
+	// }
+});
