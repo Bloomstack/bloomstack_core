e43c48495a7cf08f39b810fb6ad9c7b33b2ea234
Myuddin khatri <myuddin@digithinkit.com>
2021-09-09 14:55:17 +0530

    fix: move sales-order.py code to erpnext from bloomstack_core

--- bloomstack_core/hooks.py
+++ bloomstack_core/hooks.py
@@ -253,12 +253,6 @@ scheduler_events = {
 	],
 	"hourly": [
 		"bloomstack_core.hook_events.user.execute_bloomtrace_integration_request"
-	],
-	"daily": [
-		"bloomstack_core.hook_events.sales_order.create_sales_invoice_against_contract"
-	],
-	"daily_long": [
-		"bloomstack_core.hook_events.sales_order.update_order_status"
 	]
 }
 
