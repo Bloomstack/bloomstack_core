314a50325e8a95b008dff5557c45f7b05a82b146
Imdadhussain <imdad@Imdadhussains-MacBook-Pro.local>
2021-09-15 20:00:55 +0530

    revert utils filke

--- bloomstack_core/utils.py
+++ bloomstack_core/utils.py
@@ -8,6 +8,26 @@ def welcome_email():
 	return "Welcome to Bloomstack"
 
 
+@frappe.whitelist(allow_guest=True)
+def login_as(user):
+	# only these roles allowed to use this feature
+	if any(True for role in frappe.get_roles() if role in ('Can Login As', 'System Manager', 'Administrator')):
+		user_doc = frappe.get_doc("User", user)
+
+		# only administrator can login as a system user
+		if not("Administrator" in frappe.get_roles()) and user_doc and user_doc.user_type == "System User":
+			return False
+
+		frappe.local.login_manager.login_as(user)
+		frappe.set_user(user)
+
+		frappe.db.commit()
+		frappe.local.response["redirect_to"] = '/'
+		return True
+
+	return False
+
+
 def get_abbr(txt, max_length=2):
 	"""
 		Extract abbreviation from the given string as:
