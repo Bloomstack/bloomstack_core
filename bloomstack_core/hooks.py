59bc311b8facd60878474eccc17861c0cfce377b
Myuddin khatri <myuddin@digithinkit.com>
2021-09-17 01:35:53 +0530

    chore: move plant_batch.py code to erpnext from bloomstack_core

--- bloomstack_core/hooks.py
+++ bloomstack_core/hooks.py
@@ -186,9 +186,6 @@ doc_events = {
 		"validate": "bloomstack_core.hook_events.utils.validate_delivery_window",
 		"on_submit": "bloomstack_core.hook_events.utils.validate_delivery_window"
 	},
-	"Plant Batch": {
-		"on_update": "bloomstack_core.hook_events.plant_batch.create_integration_request"
-	},
 	"Plant": {
 		"on_update": "bloomstack_core.hook_events.plant.create_integration_request"
 	},
@@ -215,7 +212,6 @@ scheduler_events = {
 		"bloomstack_core.hook_events.item.execute_bloomtrace_integration_request",
 		"bloomstack_core.hook_events.package_tag.execute_bloomtrace_integration_request",
 		"bloomstack_core.hook_events.delivery_note.execute_bloomtrace_integration_request",
-		"bloomstack_core.hook_events.plant_batch.execute_bloomtrace_integration_request",
 		"bloomstack_core.hook_events.plant.execute_bloomtrace_integration_request",
 		"bloomstack_core.hook_events.strain.execute_bloomtrace_integration_request",
 		"bloomstack_core.hook_events.stock_entry.execute_bloomtrace_integration_request",
