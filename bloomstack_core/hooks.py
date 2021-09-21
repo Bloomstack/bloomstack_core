a95ca8dca8c035ab39b13ed72a504247471fa3b1
Myuddin Khatri <53251406+MyuddinKhatri@users.noreply.github.com>
2021-09-21 13:16:23 +0530

    chore: move item.py code to erpnext from bloomstack_core (#735)
    * chore: move item.py code to erpnext from bloomstack_core
    
    * Update bloomstack_core/hooks.py
    
    Co-authored-by: sahil28297 <37302950+sahil28297@users.noreply.github.com>

--- bloomstack_core/hooks.py
+++ bloomstack_core/hooks.py
@@ -161,9 +161,6 @@ doc_events = {
 		"on_submit" : "bloomstack_core.hook_events.delivery_trip.make_transfer_templates",
 		"on_update_after_submit": "bloomstack_core.hook_events.delivery_trip.set_vehicle_last_odometer_value",
 	},
-	"Item": {
-		"on_update": "bloomstack_core.hook_events.item.create_integration_request"
-	},
 	"Sales Invoice": {
 		"before_submit": "bloomstack_core.hook_events.sales_invoice.create_metrc_sales_receipt",
 		"before_update_after_submit": "bloomstack_core.hook_events.sales_invoice.set_invoice_status"
@@ -182,7 +179,6 @@ doc_events = {
 
 scheduler_events = {
 	"all": [
-		"bloomstack_core.hook_events.item.execute_bloomtrace_integration_request",
 		"bloomstack_core.hook_events.delivery_note.execute_bloomtrace_integration_request"
 	],
 	"hourly": [
