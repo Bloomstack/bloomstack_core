58511e697e003e834008f06d140857ffcf79e2f5
Sahil Khan <sahilkhan28297@gmail.com>
2021-09-22 15:40:33 +0530

    chore: move issues api to erpnext from bloomstack_core

--- bloomstack_core/services/issues.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-@frappe.whitelist(allow_guest=True)
-def issue_status_list():
-	"""
-	Returns an array of statuses available on the Issues doctype.
-	"""
-	meta = frappe.get_meta("Issue")
-	return meta.get_options("status").split("\n")
