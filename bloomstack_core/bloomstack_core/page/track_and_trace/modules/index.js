46e0eafe7675e0e11e61b0aab69c49a9dee2894a
Neha Sacher <45919049+nehasacher143@users.noreply.github.com>
2021-09-23 19:23:09 +0530

    chore: move the track and trace feature from bloomstack core to erpnext (#741)

--- bloomstack_core/bloomstack_core/page/track_and_trace/modules/index.js
+++
@@ -1,7 +0,0 @@
-{% include "bloomstack_core/bloomstack_core/page/track_and_trace/templates/track_and_trace_module_generic.html" %}
-{% include "bloomstack_core/bloomstack_core/page/track_and_trace/modules/serial_no.js" %}
-{% include "bloomstack_core/bloomstack_core/page/track_and_trace/modules/stock_entry.js" %}
-{% include "bloomstack_core/bloomstack_core/page/track_and_trace/modules/stock_ledger_entry.js" %}
-{% include "bloomstack_core/bloomstack_core/page/track_and_trace/modules/batch.js" %}
-{% include "bloomstack_core/bloomstack_core/page/track_and_trace/modules/delivery_note.js" %}
-{% include "bloomstack_core/bloomstack_core/page/track_and_trace/modules/purchase_receipt.js" %}
